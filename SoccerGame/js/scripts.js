45fdd51ef962fc719bde2ebff760ecb334d7baa6
Yohan Kim <yohan@yohanpro.com>
2018-08-11 19:34:05 +0900

    Three.js 연습

---
+++ SoccerGame/js/scripts.js
@@ -0,0 +1,35 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// add cube
+var geometry = new THREE.IcosahedronGeometry(20, 0);
+var material = new THREE.MeshNormalMaterial();
+var icosahedron = new THREE.Mesh( geometry, material );
+scene.add( icosahedron );
+
+// rotate cube
+icosahedron.rotation.x = 0.1;
+icosahedron.rotation.y = -0.25;
+
+camera.position.z = 550;
+
+var light = new THREE.PointLight( 0xFFFF00 );
+light.position.set( 10, 0, 25 );
+scene.add( light );
+scene.background = new THREE.Color(0xfffff);
+
+var render = function () {
+  requestAnimationFrame( render );
+
+  renderer.render(scene, camera);
+  
+  icosahedron.rotation.x += 0.05;
+};
+
+render();
+
+
