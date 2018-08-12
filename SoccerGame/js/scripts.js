ffda51369a3ddb6793fa8c3bcec900337b18e147
Yohan Kim <yohan@yohanpro.com>
2018-08-12 20:01:20 +0900

    공 궤적

--- SoccerGame/js/scripts.js
+++ SoccerGame/js/scripts.js
@@ -1,35 +1,36 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
 
-// add cube
-var geometry = new THREE.IcosahedronGeometry(20, 0);
-var material = new THREE.MeshNormalMaterial();
-var icosahedron = new THREE.Mesh( geometry, material );
-scene.add( icosahedron );
+// // add cube
+// var geometry = new THREE.IcosahedronGeometry(20, 0);
+// var material = new THREE.MeshNormalMaterial();
 
-// rotate cube
-icosahedron.rotation.x = 0.1;
-icosahedron.rotation.y = -0.25;
+// var icosahedron = new THREE.Mesh( geometry, material );
+// scene.add( icosahedron );
 
-camera.position.z = 550;
+// // rotate cube
+// icosahedron.rotation.x = 0.1;
+// icosahedron.rotation.y = -0.25;
 
-var light = new THREE.PointLight( 0xFFFF00 );
-light.position.set( 10, 0, 25 );
-scene.add( light );
-scene.background = new THREE.Color(0xfffff);
+// camera.position.z = 550;
 
-var render = function () {
-  requestAnimationFrame( render );
+// var light = new THREE.PointLight( 0xFFFF00 );
+// light.position.set( 10, 0, 25 );
+// scene.add( light );
+// scene.background = new THREE.Color(0xffff01);
 
-  renderer.render(scene, camera);
+// var render = function () {
+//   requestAnimationFrame( render );
+
+//   renderer.render(scene, camera);
   
-  icosahedron.rotation.x += 0.05;
-};
+//   icosahedron.rotation.x += 0.05;
+// };
 
-render();
+// render();
 
 
